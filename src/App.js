c46a999c83ba5c51ca52736de3c418f6302ebcd9
Daniel Targonski <daniel.targonski2@gmail.com>
2021-01-14 18:42:50 -0500

    zipcode or city-name search for info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Search from "./Components/Search";
+import "./App.css";
+import SearchCity from "./Components/SearchCity";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zip: [],
+      cityName: [],
+    };
+    this.setter = this.setData.bind(this);
+    this.setterCity = this.setCityName.bind(this);
+  }
+
+  setCityName = (value) => {
+    this.setState({ cityName: value });
+    console.log(value);
+  };
+
+  setData = (value) => {
+    this.setState({ zip: value });
+    console.log(value);
+  };
+
+  render() {
+    let displayZipArr;
+    let displayCityArr;
+
+    if (this.state.zip.length > 0) {
+      displayZipArr = this.state.zip.map((city) => {
+        return <div>{city.City}</div>;
+      });
+    } else {
+      displayZipArr = <div>No Results</div>;
+    }
+
+    if (this.state.cityName.length > 0) {
+      displayCityArr = this.state.cityName.map((city) => {
+        return <div>{city}</div>;
+      });
+    } else {
+      displayCityArr = <div>No Results</div>;
+    }
+
+    return (
+      <div className="App">
+        <Search setter={this.setter}></Search>
+        {displayZipArr}
+        <SearchCity setter={this.setterCity}></SearchCity>
+        {displayCityArr}
+      </div>
+    );
+  }
 }
 
 export default App;
